a91ebc20198a8123d2978e7d0126c61bccfcf47a
Stevenn Muller <stevenn.muller@gmai.com>
2013-04-08 20:47:24 -0300

    CommitFInal
    Tentou-se implementar notificações, esta parcialmente funcionando. Mas
    ainda não esta seguro!!

--- Biblioteca/gen/br/rafiki/bibliotecaAndroid/R.java
+++ Biblioteca/gen/br/rafiki/bibliotecaAndroid/R.java
@@ -15,50 +15,70 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int pergamun=0x7f020001;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f08000a;
-        public static final int TableLayout1=0x7f08000c;
-        public static final int button1=0x7f080013;
-        public static final int etPesquisa=0x7f080010;
+        public static final int RelativeLayout1=0x7f08000c;
+        public static final int TableLayout1=0x7f08000a;
+        public static final int btBusca=0x7f080020;
+        public static final int btLogin=0x7f08001b;
+        public static final int button1=0x7f08001a;
+        public static final int edMatricula=0x7f080013;
+        public static final int edSenha=0x7f080017;
+        public static final int etPesquisa=0x7f08001f;
         public static final int linearLayout1=0x7f080000;
-        public static final int lvResultados=0x7f08000b;
-        public static final int menu_settings=0x7f080014;
+        public static final int lvResultados=0x7f08000d;
+        public static final int menu_settings=0x7f080021;
         public static final int scrollView1=0x7f080008;
-        public static final int spTpBusca=0x7f08000e;
+        public static final int spTpBusca=0x7f08001d;
         public static final int tableDetalhes=0x7f080001;
         public static final int tableRow1=0x7f080002;
         public static final int tableRow2=0x7f080005;
-        public static final int tableRow3=0x7f080011;
+        public static final int tableRow3=0x7f080018;
+        public static final int tableRow4=0x7f08000e;
+        public static final int tableRow5=0x7f080010;
+        public static final int tableRow6=0x7f080014;
         public static final int textView1=0x7f080012;
-        public static final int tvBusca=0x7f08000f;
+        public static final int textView2=0x7f080016;
+        public static final int textView3=0x7f080019;
+        public static final int textView4=0x7f08000f;
+        public static final int textView5=0x7f080011;
+        public static final int textView6=0x7f080015;
+        public static final int tvBusca=0x7f08001e;
         public static final int tvDispValor=0x7f080009;
         public static final int tvEdiValor=0x7f080007;
         public static final int tvEditora=0x7f080006;
-        public static final int tvTipo=0x7f08000d;
+        public static final int tvEmprestimo=0x7f08000b;
+        public static final int tvTipo=0x7f08001c;
         public static final int tvTitValor=0x7f080004;
         public static final int tvTitulo=0x7f080003;
     }
     public static final class layout {
         public static final int activity_detalhes=0x7f030000;
-        public static final int activity_exibir_resultados=0x7f030001;
-        public static final int activity_main=0x7f030002;
+        public static final int activity_emprestimo=0x7f030001;
+        public static final int activity_exibir_resultados=0x7f030002;
+        public static final int activity_login=0x7f030003;
+        public static final int activity_main=0x7f030004;
     }
     public static final class menu {
         public static final int activity_detalhes=0x7f070000;
-        public static final int activity_exibir_resultados=0x7f070001;
-        public static final int activity_main=0x7f070002;
+        public static final int activity_emprestimo=0x7f070001;
+        public static final int activity_exibir_resultados=0x7f070002;
+        public static final int activity_login=0x7f070003;
+        public static final int activity_main=0x7f070004;
     }
     public static final class string {
-        public static final int Busca=0x7f040003;
+        public static final int Busca=0x7f040002;
         public static final int app_name=0x7f040000;
-        public static final int etPesquisa_text=0x7f040006;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_detalhes=0x7f040008;
-        public static final int title_activity_exibir_resultados=0x7f040007;
-        public static final int tvBusca_text=0x7f040005;
-        public static final int tvTitulo_text=0x7f040004;
+        public static final int btLogin_label=0x7f04000a;
+        public static final int etPesquisa_text=0x7f040005;
+        public static final int menu_settings=0x7f040001;
+        public static final int title_activity_detalhes=0x7f040007;
+        public static final int title_activity_emprestimo=0x7f040009;
+        public static final int title_activity_exibir_resultados=0x7f040006;
+        public static final int title_activity_login=0x7f040008;
+        public static final int tvBusca_text=0x7f040004;
+        public static final int tvTitulo_text=0x7f040003;
     }
     public static final class style {
         /** 
--- Biblioteca/src/br/rafiki/bibliotecaAndroid/ExibirResultados.java
+++ Biblioteca/src/br/rafiki/bibliotecaAndroid/ExibirResultados.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -16,7 +17,12 @@ public class ExibirResultados extends Activity {
 	private ResultadosAdapter listAdapter;
 	private List<Resultado> resultados;
 	private ListView lvResultado;
-	Intent intent;
+	private static final String STATUS_THREAD_DETALHES = "br.rafiki.androidBiblioteca.STATUS_THREAD_DETALHES";
+	private static final String RESULTADO = "br.rafiki.androidBiblioteca.RESULTADO";
+	private Intent intent;
+	private ParserDetails taskDetalhes;
+	private Resultado resultado;
+	private String Id;
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -35,11 +41,10 @@ public class ExibirResultados extends Activity {
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				Resultado resultado = (Resultado) lvResultado
-						.getItemAtPosition(arg2);
-
-				new ParserDetails(ExibirResultados.this).execute(resultado
-						.getId());
+				resultado = (Resultado) lvResultado.getItemAtPosition(arg2);
+				Id=resultado.getId();
+				taskDetalhes = (ParserDetails) new ParserDetails(
+						ExibirResultados.this).execute(resultado.getId());
 
 			}
 		});
@@ -52,4 +57,36 @@ public class ExibirResultados extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+		final ParserDetails task = taskDetalhes;
+		if (task != null && task.getStatus() != AsyncTask.Status.FINISHED) {
+			outState.putBoolean(STATUS_THREAD_DETALHES, true);
+			outState.putString(RESULTADO, Id);
+			task.cancel(true);
+		}
+		taskDetalhes = null;
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onRestoreInstanceState(savedInstanceState);
+		if (savedInstanceState.getBoolean(STATUS_THREAD_DETALHES)) {
+			Id = savedInstanceState.getString(RESULTADO);
+			taskDetalhes = (ParserDetails) new ParserDetails(
+					ExibirResultados.this).execute(Id);
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		if (taskDetalhes != null
+				&& taskDetalhes.getStatus() == AsyncTask.Status.RUNNING)
+			taskDetalhes.cancel(true);
+	}
 }
